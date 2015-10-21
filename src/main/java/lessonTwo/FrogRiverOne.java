9f2f44cdf13a8dad3bb2578f1560ca9c0e5ab184
ten029 <trent.jones@capitalone.com>
2015-10-21 08:32:16 -0500

    code clean up

--- src/main/java/lessonTwo/FrogRiverOne.java
+++ src/main/java/lessonTwo/FrogRiverOne.java
@@ -22,9 +22,7 @@ public class FrogRiverOne {
 	public static void main(String[] args) {
 		
 		int [] A = {1, 3, 1, 4, 2, 3, 5, 4};
-		
 		System.out.println(new FrogRiverOne().solution(5, A));
-
 	}
 
 }
