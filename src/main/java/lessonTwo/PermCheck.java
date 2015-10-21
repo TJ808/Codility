eef672b2662e51b586405dc29c67e90daa76c39a
ten029 <trent.jones@capitalone.com>
2015-10-21 09:32:11 -0500

    added comments to PermCheck solution

--- src/main/java/lessonTwo/PermCheck.java
+++ src/main/java/lessonTwo/PermCheck.java
@@ -4,11 +4,23 @@ public class PermCheck {
 
 	public int solution(int [] A) {
 		
+		//Store size of Array A
 		int size = A.length;
+		
+		//Declare and intialize counter array with size equal to array A
 		int [] counter = new int[size];
 		
 		for(int i = 0; i < size; ++i) {
 
+			/* Since the problem states a permutation contains each element
+			from 1 to N once and only once, we have two checks. The first is
+			to check if any element in array A has a value greater than the size
+			of array A. If it does this is not a permutation since a permutation
+			must start with 1. If an elements value is greater than the number of 
+			elements in the array, a permutation is not possible. The second check
+			makes sure each element only appears once. If it appears more than once
+			it is not a permutation */
+			
 			if(A[i] <= size && counter[ A[i] - 1] == 0) {
 				counter[ A[i] - 1] = 1;
 			}
