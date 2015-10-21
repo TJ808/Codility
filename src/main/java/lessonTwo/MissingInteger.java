91882ce9e444e5fb702a9b2fdf1611a7dabfed84
Trent Jones <trent.jones@capitalone.com>
2015-10-21 10:17:50 -0500

    MissingInteger Solution

---
+++ src/main/java/lessonTwo/MissingInteger.java
@@ -0,0 +1,15 @@
+package lessonTwo;
+
+public class MissingInteger {
+	
+	public int solution(int[] A) {
+		
+		return -1;
+	}
+
+	public static void main(String[] args) {
+
+		int [] A = {1, 3, 6, 4, 1, 2};
+		System.out.println(new MissingInteger().solution(A));
+	}
+}
