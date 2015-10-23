90451a4ecb65e67cfafd0ebb9f3a43b7198271b2
Trent Jones <tjonesx7@gmail.com>
2015-10-23 09:13:31 -0500

    added comments

---
+++ src/main/java/lessonTwo/MaxCounters.java
@@ -0,0 +1,10 @@
+package lessonTwo;
+
+public class MaxCounters {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/main/java/lessonTwo/MissingInteger.java
+++ src/main/java/lessonTwo/MissingInteger.java
@@ -9,20 +9,15 @@ public class MissingInteger {
 		/* populate counter array. We only care about positive numbers and numbers
 		 * less than or equal to the length of the original array. Numbers with values
 		 * greater than the original array are irrelevant since it won't be possible
-		 * to reach them without skipping a lower number first.
-		 */
-		
+		 * to reach them without skipping a lower number first. */
 		for(int i = 0; i < A.length; ++i) {
 			if(A[i] > 0 && A[i] <= A.length && counter[ A[i] ] == false) {
 				counter[ A[i] ] = true;
 			}
 		}
 		
-		
 		/* loop through counter array. First element at position j that is
-		 * not true is the minimal missing integer
-		 */
-		
+		 * not true is the minimal missing integer */	
 		for(int j = 1; j < counter.length; ++j) {
 			if(counter[j] != true) {
 				return j;
